8cd57ae961c1d70912f808f9f8b3db2d67b332bd
Chris McComb <ccmcc2012@gmail.com>
2025-11-22 11:57:53 -0500

    Ensure generated scripts pass shfmt

--- stubs/alpha_stub.zsh
+++ stubs/alpha_stub.zsh
@@ -1,23 +1,26 @@
 #!/usr/bin/env zsh
 
 function heading() {
-  local heading="$1"
-  local color_start
-  local color_end
-  local width=80  # Width of the block
-  local padding
+	local heading="$1"
+	local color_start
+	local color_end
+	local width=80 # Width of the block
+	local padding
 
-  # Define colors
-  color_start=$(tput setab 5; tput setaf 0)  # Purple background, black text
-  color_end=$(tput sgr0)                     # Reset text
+	# Define colors
+	color_start=$(
+		tput setab 5
+		tput setaf 0
+	)                      # Purple background, black text
+	color_end=$(tput sgr0) # Reset text
 
-  # Calculate padding for centering the heading
-  padding=$(( (width - ${#heading}) / 2 ))
+	# Calculate padding for centering the heading
+	padding=$(((width - ${#heading}) / 2))
 
-  # Print the heading with a colored block
-  echo
-  printf "${color_start}%*s${color_end}\n" "$width" ""
-  printf "${color_start}%*s%s%*s${color_end}\n" "$padding" "" "$heading" "$padding" ""
-  printf "${color_start}%*s${color_end}\n" "$width" ""
-  echo
+	# Print the heading with a colored block
+	echo
+	printf "${color_start}%*s${color_end}\n" "$width" ""
+	printf "${color_start}%*s%s%*s${color_end}\n" "$padding" "" "$heading" "$padding" ""
+	printf "${color_start}%*s${color_end}\n" "$width" ""
+	echo
 }
